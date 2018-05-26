dc5215cd8b5c1f1a4ef03e1aae048a295a91799e
Anthony C. Scott Jr <acsii@me.com>
2018-05-26 12:02:27 -0400

    Added config, public, and database folders to complete the MVC file structure

---
+++ app/controllers/api-users.js
@@ -0,0 +1,28 @@
+var userData = require ('../models/user');
+
+
+module.exports = function (app) {
+
+    app.get('/api/users', function(req,res) {
+        res.json(userData);
+    })
+
+    // app.get('/api/waitlist', function(req,res) {
+    //     res.json(waitingListData);
+    // })
+    //
+    // app.post('/api/tables', function (req, res) {
+    //     if(tableData.length < 5) {
+    //         tableData.push(req.body);
+    //         res.json(true);
+    //     } else {
+    //         waitingListData.push(req.body);
+    //         res.json(false);
+    //     }
+    // })
+    //
+    // app.post('/api/clear', function(){
+    //     tableData = [];
+    //     waitingListData = [];
+    // })
+};
---
+++ app/models/user.js
@@ -0,0 +1,15 @@
+var userArray = [
+    {
+        userName: 'Tony',
+        password: 'noproblems',
+        paymentForm: 'creditCard',
+        nameOnCC: 'Tony Monaco',
+        ccNumber: '000000000000',
+        ccType: 'Amex',
+        ccExp:'01/22',
+        ccZip:'90210',
+        favCrypto:'6',
+    }
+]
+
+module.exports = userArray;
